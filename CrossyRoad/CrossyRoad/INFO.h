678f1aa955b51092ed25db350f5a8343771c895e
kimjunhyok(MSILeopardPro) <kimjunhyok@naver.com>
2018-11-09 21:32:47 +0900

    making Framework
    야진짜1주일잡고해야할듯 readme읽어봐

---
+++ CrossyRoad/CrossyRoad/INFO.h
@@ -0,0 +1,7 @@
+#pragma once
+typedef struct __info
+{
+	float x;
+	float y;
+	float z;
+} INFO;
---
+++ CrossyRoad/CrossyRoad/Object.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Object.h"
+
+
+Object::Object()
+	:info({}),speed(0)
+{
+}
+
+
+Object::~Object()
+{
+}
+
